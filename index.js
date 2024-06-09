f7d8f71c39497ea8af4b2d315fa69f0592130241
jewel1269 <jewelmia2330@gamil.com>
2024-06-09 18:37:33 +0600

    finaly complete

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri =
-  'mongodb+srv://juyelhabib272732:rz3E3tP7oDEykPMv@cluster0.gwpu1ni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.gwpu1ni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
